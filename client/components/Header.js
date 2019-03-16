d523f08e1d58aad583550a9b22643cfa22962ec1
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2019-03-16 04:33:10 -0700

    Login and Logout buttons work

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
 import {graphql} from 'react-apollo';
 import CurrentUser from '../queries/CurrentUser';
+import {Link} from 'react-router';
 
 class Header extends Component {
+  renderButtons() {
+    const {loading, user} = this.props.data;
+    if (loading) return <div />;
+
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  }
+
   render() {
     console.log(this.props);
     if (this.props.data.laoding) return <div>Loading...</div>;
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
