16ca006947651a256072ce62a44bbb578ea4e6c7
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2019-03-15 21:05:41 -0700

    Setup query with the header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import {graphql} from 'react-apollo';
+import CurrentUser from '../queries/CurrentUser';
 
 class Header extends Component {
   render() {
+    console.log(this.props);
+    if (this.props.data.laoding) return <div>Loading...</div>;
     return <div>Header</div>;
   }
 }
 
-export default Header;
+export default graphql(CurrentUser)(Header);
