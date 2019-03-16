0a704a3f2c59f24b6a6be2b737b939fed4bb8dc2
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2019-03-15 18:57:26 -0700

    Test

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,8 +1,22 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const {GraphQLObjectType, GraphQLID} = graphql;
+const UserType = require('./user_type');
 
+// by default gql alway exptects at least one field available
+// for defined type
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+  name: 'RootQueryType',
+  fields: {
+    dummyField: {type: GraphQLID},
+    user: {
+      // checking user authenticated
+      type: UserType,
+      resolve(parentValue, args, req) {
+        // req connected with password
+        return req.user;
+      },
+    },
+  },
 });
 
 module.exports = RootQueryType;
